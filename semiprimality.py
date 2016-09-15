81339559286f6fb41ca3370a99b67232c29cc4a4
Roman Korolev <zak.mamontov@mail.ru>
2016-09-15 13:42:41 +0300

    command line argument

--- semiprimality.py
+++ semiprimality.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import sys
 import math
 import random
 from itertools import islice, cycle, count, compress
 
-N = 100000
+N = int(sys.argv[1])
 
 
 def croft():  # спираль Крофта
@@ -44,9 +44,10 @@ def decompose(n):  # собственно, бьём на множители
 def is_semiprime(n):  # алгоритм проверки на полупростоту
     d = decompose(n)
     try:
-        return next(d) * next(d) == n
+        a, b = next(d), next(d)
+        return a*b == n, a, b
     except:
-        return False
+        return False, 0, 0
 
 
 def is_prime(n):  # алгоритм проверки на пустоту, использующий теперь спираль Крофта
@@ -74,14 +75,16 @@ def main():  # самая некрасивая функция из-за свои
     num = newton(N, f, f1, 1)  # нашли число, которое должно быть N-м по счёту. Ландау ручается, что так оно и есть :)
     num = int(math.floor(num))
     delta = 0
+    res = [False, ]
     while not discovered:
-        if is_semiprime(num):
+        res = is_semiprime(num)
+        if res[0]:
             discovered = True
         else:
             num = num + delta  # количество полупростых высчитывается не точно, соответсвуюющее число нужное число может оказаться в небольшом радиусе
             delta = -1*(abs(delta)+1)*(delta/abs(delta)) if delta else 1  # иногда нужно пометаться, если не натолкнулись на нужное число
 
-    print num
+    print(num, res[1], res[2])
 
 if __name__ == '__main__':
     main()
